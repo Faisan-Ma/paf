e5b97e08698605f72d1e3c6672d46066ed3ae3bb
ajanthanajan66 <ajanthanajan@gmail.com>
2022-04-26 10:45:41 +0530

    change insert

--- src/main/java/model/PowerUnit.java
+++ src/main/java/model/PowerUnit.java
@@ -31,10 +31,18 @@ public class PowerUnit {
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
+	public String getMonth() {
+		return month;
+	}
+	public void setMonth(String month) {
+		this.month = month;
+	}
 	private int unitid;
 	private int accountNo;
 	private String CusName;
 	private int unit;
+	private String month;
 	private int amount;
+	
 
 }
